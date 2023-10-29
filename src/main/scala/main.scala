52b788a4a474dbfecaa5fc8c9004c2b9d79c87d0
Sisir Koppaka <sisir.koppaka@gmail.com>
2023-10-29 15:59:04 +0530

    add Payload for actual prompt text

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,18 +2,27 @@ package digital.blueinsight.funscala2023
 
 object SamplePromptInstruction:
   import Prompts._
-  def summarizePromptInstruction =
-    Seq(PromptMessage.SummarizeDirection, PromptMessage.CreativeDirection, PromptMessage.TrueToTextDirection)
+  def summarizePromptInstruction: Seq[PromptMessage] =
+    Seq(PromptMessage.SummarizeDirection, 
+    PromptMessage.CreativeDirection, 
+    PromptMessage.TrueToTextDirection, 
+    PromptMessage.PayloadDirection(Payload("\n```The weather was super hot, and it made the travellers dreary and forlorn.```")))
 end SamplePromptInstruction
 
 object Prompts:
-  enum PromptMessage(message: String):
-    case SummarizeDirection extends PromptMessage("""Summarize the below text into a short sentence of less than 15 words.""")
+  enum PromptMessage(message: String=""""""):
+    case SummarizeDirection extends PromptMessage("""Summarize the below text enclosed within triple backticks into a short sentence of less than 15 words.""")
     case CreativeDirection extends PromptMessage("""Be creative.""")
     case TrueToTextDirection extends PromptMessage("""Be as close to the text below as possible.""")
+    case PayloadDirection(p: Payload) extends PromptMessage()
 
-    def getMessage: String = message
+    def getMessage: String = this match {
+      case PayloadDirection(p) => p.p
+      case _ => message
+    }
   end PromptMessage
+
+  case class Payload(p: String = "") 
 
   def getPrompt(promptMessages: Seq[PromptMessage]): String = 
     promptMessages.map(_.getMessage).mkString(" ")
