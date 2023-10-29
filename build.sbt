350513b0539403be790888bb2c32804bf27ce364
Sisir Koppaka <sisir.koppaka@gmail.com>
2023-10-29 21:03:56 +0530

    introduce zioappdefault

--- build.sbt
+++ build.sbt
@@ -2,6 +2,10 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "3.3.1"
 
+val zioSchemaVersion = "0.4.9"
+val zioVersion = "2.0.10"
+val zioOpenAiVersion = "0.2.0"
+
 lazy val root = (project in file("."))
   .settings(
     name := "funscala2023",
@@ -9,4 +13,16 @@ lazy val root = (project in file("."))
   )
 
 libraryDependencies += "io.cequence" %% "openai-scala-client" % "0.4.1"
-libraryDependencies += ("com.typesafe.akka" %% "akka-actor" % "2.6.20").cross(CrossVersion.for3Use2_13)
+libraryDependencies += ("com.typesafe.akka" %% "akka-actor" % "2.6.20").cross(
+  CrossVersion.for3Use2_13
+)
+
+libraryDependencies ++= Seq(
+  "dev.zio" %% "zio" % zioVersion,
+  "dev.zio" %% "zio-test" % zioVersion % Test,
+  "dev.zio" %% "zio-test-sbt" % zioVersion % Test,
+  "dev.zio" %% "zio-openai" % zioOpenAiVersion,
+  "dev.zio" %% "zio-schema" % zioSchemaVersion,
+  "dev.zio" %% "zio-schema-json" % zioSchemaVersion,
+  "com.lihaoyi" %% "pprint" % "0.8.1"
+)
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,35 +1,70 @@
 package digital.blueinsight.funscala2023
 
+import zio.*
+import zio.http.ZClient
+import zio.http.model.Status
+import zio.json.*
+import zio.openai.Chat
+import zio.openai.model.{ChatCompletionRequestMessage, OpenAIFailure, Role}
+import zio.schema.Schema
+
 object SamplePromptInstruction:
   import Prompts._
   def summarizePromptInstruction: Seq[PromptMessage] =
-    Seq(PromptMessage.SummarizeDirection, 
-    PromptMessage.CreativeDirection, 
-    PromptMessage.TrueToTextDirection, 
-    PromptMessage.CutoffReferenceTime(Payload("1950")),
-    PromptMessage.PayloadDirection(Payload("\n```The weather was super hot, and it made the travellers dreary and forlorn.```")))
+    Seq(
+      PromptMessage.SummarizeDirection,
+      PromptMessage.CreativeDirection,
+      PromptMessage.TrueToTextDirection,
+      PromptMessage.CutoffReferenceTime(Payload("1950")),
+      PromptMessage.PayloadDirection(
+        Payload(
+          "\n```The weather was super hot, and it made the travellers dreary and forlorn.```"
+        )
+      )
+    )
 end SamplePromptInstruction
 
 object Prompts:
-  enum PromptMessage(message: String=""""""):
-    case SummarizeDirection extends PromptMessage("""Summarize the below text enclosed within triple backticks into a short sentence of less than 15 words.""")
+  enum PromptMessage(message: String = """"""):
+    case PreambleDirection(p: Payload) extends PromptMessage()
+    case SummarizeDirection
+        extends PromptMessage(
+          """Summarize the below text enclosed within triple backticks into a short sentence of less than 15 words."""
+        )
     case CreativeDirection extends PromptMessage("""Be creative.""")
-    case TrueToTextDirection extends PromptMessage("""Be as close to the text below as possible.""")
-    case CutoffReferenceTime(p: Payload) extends PromptMessage(s"""Do not refer to any incident before the date mentioned here: ${p.p}.""")
+    case TrueToTextDirection
+        extends PromptMessage("""Be as close to the text below as possible.""")
+    case CutoffReferenceTime(p: Payload)
+        extends PromptMessage(
+          s"""Do not refer to any incident before the date mentioned here: ${p.p}."""
+        )
     case PayloadDirection(p: Payload) extends PromptMessage()
 
     def getMessage: String = this match {
-      case PayloadDirection(p) => p.p
-      case _ => message
+      case PreambleDirection(p) => p.p
+      case PayloadDirection(p)  => p.p
+      case _                    => message
     }
   end PromptMessage
 
-  case class Payload(p: String = "") 
+  case class Payload(p: String = "")
 
-  def getPrompt(promptMessages: Seq[PromptMessage]): String = 
+  def getPrompt(promptMessages: Seq[PromptMessage]): String =
     promptMessages.map(_.getMessage).mkString(" ")
 
-  @main def basicPrompt() = {
-    println(getPrompt(SamplePromptInstruction.summarizePromptInstruction))
+  def basicPrompt(): Task[String] = {
+    ZIO.succeed(getPrompt(SamplePromptInstruction.summarizePromptInstruction))
   }
 end Prompts
+
+object MainApp extends ZIOAppDefault {
+  val myApp: ZIO[Any, Throwable, Unit] =
+    for {
+      date <- Clock.currentDateTime
+      _ <- ZIO.logInfo(s"Application started at $date")
+      result <- Prompts.basicPrompt()
+      _ <- Console.printLine(result)
+    } yield ()
+
+  def run = myApp
+}
